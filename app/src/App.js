132ae81672796ce198e728a696490c901ee906e9
Vladislav <zhukov.vlaad@mail.ru>
2023-04-13 00:03:18 +0700

    init

---
+++ app/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import Main from './Pages/Main';
+
+function App() {
+
+  return (
+    <div className="App">
+      <Routes>
+        <Route path='/' element={<Main/>}/>
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
